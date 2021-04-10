3b2363f0407c1f057970756a372a4ff8d259849d
ashante112 <74433556+ashante112@users.noreply.github.com>
2021-04-10 14:04:35 -0400

    Updates

--- EmployeeSQL/data/schema.sql
+++ EmployeeSQL/data/schema.sql
@@ -1,26 +1,40 @@
 
-DROP TABLE employees;
-DROP TABLE titles;
+-- DROP TABLE employees;
+-- DROP TABLE titles;
 
-CREATE TABLE titles (
-  title_id varchar,
-  title varchar,
-  Primary Key (title_id)
-);
+-- CREATE TABLE titles (
+--   title_id varchar,
+--   title varchar,
+--   Primary Key (title_id)
+-- );
 
-CREATE TABLE employees (
-  emp_no int,
-  emp_title_id varchar,
-  birth_date date,
-  first_name varchar,
-  last_name varchar,
-  sex varchar,
-  hire_date date,
-  Foreign Key (emp_title_id) references titles(title_id),
-  Primary Key (emp_no)
-);
+-- CREATE TABLE employees (
+--   emp_no int,
+--   emp_title_id varchar,
+--   birth_date date,
+--   first_name varchar,
+--   last_name varchar,
+--   sex varchar,
+--   hire_date date,
+--   Foreign Key (emp_title_id) references titles(title_id),
+--   Primary Key (emp_no)
+-- );
 
-SELECT *
-FROM employees;
+-- SELECT *
+-- FROM employees;
 
-
+-- CREATE TABLE salaries(
+--     emp_no INT PRIMARY KEY,
+-- 	salary INT NOT NULL
+-- 	);
+	
+-- CREATE TABLE departments(
+--     dept_no INT PRIMARY KEY,
+-- 	dept_name VARCHAR(30) NOT NULL
+-- 	);
+	
+CREATE TABLE dept_manager(
+    dept_no VARCHAR,
+	emp_no INT PRIMARY KEY,
+	);
+	
